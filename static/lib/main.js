49cd1ccbb86ddadfedc2b1f05b3ef6d1fade5b7e
BirdZhang <0312birdzhang@gmail.com>
2018-10-08 17:03:43 +0800

    修复异步加载新页面出现重复图标的bug

--- static/lib/main.js
+++ static/lib/main.js
@@ -61,11 +61,12 @@ $(document).ready(function () {
 		var posts = pid ? '[component="post"][data-pid="' + pid + '"]' : '[component="post"]';
 		$(posts).each(function () {
 			var post = $(this);
-            if(!post.hasClass("nodebb-plugin-share-post-icons-cn")){
-                app.parseAndTranslate('partials/nodebb-plugin-share-post-icons-cn/share', {}, function (tpl) {
-                    $(tpl).insertBefore(post.find('.post-tools'));
-                });
-            }
+			var share_icons_cn = $('[class="pull-right"]');
+        	if(!share_icons_cn.hasClass("nodebb-plugin-share-post-icons-cn")){
+				app.parseAndTranslate('partials/nodebb-plugin-share-post-icons-cn/share', {}, function (tpl) {
+					$(tpl).insertBefore(post.find('.post-tools'));
+				});
+			}
 		});
 	}
 });
